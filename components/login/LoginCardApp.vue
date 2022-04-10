7e454ea5d34a6fc4ec95a3386241ef52b3771116
pedrosantos <peterpedrods@gmail.com>
2022-04-10 11:28:22 -0300

    fix height

--- components/login/LoginCardApp.vue
+++ components/login/LoginCardApp.vue
@@ -103,7 +103,7 @@
           <button
             type="submit"
             :disabled="isLoading"
-            class="py-2 px-4 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+            class="py-2 px-4 h-10 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
           >
             <fa-icon v-if="isLoading" :icon="iconSpinner" class="spinner" />
             <span v-else> Login </span>
