2373ed404284fd510d0ab6e43f93e58e38d3daa4
pedro <peterpedrods@gmail.com>
2023-04-26 20:33:07 -0300

    add test to switchtoggle

--- components/base/SwitchToggle/__tests__/SwitchToggle.spec.ts
+++ components/base/SwitchToggle/__tests__/SwitchToggle.spec.ts
@@ -1,16 +1,38 @@
-import { describe, it, expect } from 'vitest'
-import { render } from '@testing-library/vue'
+import { describe, it, expect, beforeEach } from 'vitest'
+import { fireEvent, render } from '@testing-library/vue'
 import SwitchToggleVue from '@/components/base/SwitchToggle/SwitchToggle.vue'
 
 describe('SwitchToggleVue', () => {
-	it('should render properly', () => {
-		const value = false
-		const wrapper = render(SwitchToggleVue, {
+	beforeEach(() => {
+		document.body.innerHTML = ''
+	})
+	it('should render properly without default state', () => {
+		const { html } = render(SwitchToggleVue, {
 			props: {
 				id: 'toggle',
-				onToggleSwitch: () => value != value,
 			},
 		})
-		expect(wrapper.html()).toMatchSnapshot()
+		expect(html()).toMatchSnapshot()
+	})
+	it('should render properly with default state', () => {
+		const { html } = render(SwitchToggleVue, {
+			props: {
+				id: 'toggle',
+				defaultState: true,
+			},
+		})
+		expect(html()).toMatchSnapshot()
+	})
+	it('should emit toggle when click', async () => {
+		const { container, emitted } = render(SwitchToggleVue, {
+			props: {
+				id: 'toggle',
+			},
+		})
+		const input = container.querySelector('input[type="checkbox"]') as HTMLInputElement
+		await fireEvent.click(input)
+		expect(emitted().toggle).toBeTruthy()
+		expect(emitted().toggle.length).toBe(1)
+		expect(emitted().toggle.values().next().value).toBeTruthy()
 	})
 })
