1558381890bf81422849d3cfd0f41908ef24e39b
pedro <peterpedrods@gmail.com>
2023-05-20 11:57:09 -0300

    single copy

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -15,6 +15,11 @@ const route = useRoute()
 const postStore = usePostsStore()
 const { currentPost: post } = storeToRefs(postStore)
 const isLoading = ref(true)
+const textToCopy = ref('')
+const el = ref<HTMLDivElement | null>(null)
+const listening = ref(false)
+const multipleSelectActive = ref(false)
+const { copy } = useClipboard({ source: textToCopy, legacy: true })
 if (route.params.id) {
 	postStore.getById(+route.params.id.toString()).finally(() =>
 		setTimeout(() => {
@@ -22,14 +27,37 @@ if (route.params.id) {
 		}, 325)
 	)
 }
+watchEffect(() => {
+	if (el.value && !listening.value) {
+		listening.value = true
+		document.body.querySelectorAll('a[href^="magnet:?"]').forEach((elLink) => {
+			elLink.addEventListener('click', (e) => {
+				e.preventDefault()
+				e.stopPropagation()
+				e.stopImmediatePropagation()
+				copyTextToClipboard((e.currentTarget as HTMLLinkElement).href)
+			})
+		})
+	}
+})
+function copyTextToClipboard(text?: string): void {
+	textToCopy.value += text
+	textToCopy.value += multipleSelectActive.value ? '\n' : ''
+	if (!text || !multipleSelectActive.value) {
+		copy(textToCopy.value)
+		textToCopy.value = ''
+	}
+}
 </script>
 
 <template>
-	<transition class="px-5 py-2">
-		<NuxtIcon v-if="isLoading" class="spinner" name="spinner" />
-		<div v-else-if="post && !isLoading" v-html="post.content.rendered"></div>
-		<div v-else-if="!isLoading && !post">Not Found</div>
-	</transition>
+	<div class="px-5 py-2">
+		<NuxtIcon v-show="isLoading" class="spinner" name="spinner" />
+		<transition>
+			<div v-if="post && !isLoading" ref="el" v-html="post.content.rendered"></div>
+			<div v-else-if="!isLoading && !post">Not Found</div>
+		</transition>
+	</div>
 </template>
 
 <style lang="scss" scoped>
