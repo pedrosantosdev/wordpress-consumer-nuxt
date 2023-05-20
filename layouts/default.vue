27b0fe2a2c62e73ba93c7f421ad5a690d5ffa299
pedro <peterpedrods@gmail.com>
2023-05-20 20:22:14 -0300

    init  toast

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import BaseNavbar from '@/components/base/BaseNavbar.vue'
+import BaseToast from '@/components/base/BaseToast.vue'
+
 const hide = computed(() => useRoute().name === 'login')
 </script>
 <template>
 	<div class="main-content">
 		<BaseNavbar v-if="!hide" />
 		<slot />
+		<BaseToast />
 	</div>
 </template>
---
+++ state/toast.ts
@@ -0,0 +1,38 @@
+import { defineStore, acceptHMRUpdate } from 'pinia'
+
+interface Toast {
+	message: string
+	status: 'success' | 'warning' | 'error' | 'default'
+	timeout: number
+}
+export interface StateModel {
+	list: Toast[]
+}
+
+const defaultShowTime = 2_000
+
+export const useToastStore = defineStore('toast', {
+	state: (): StateModel => ({
+		list: [],
+	}),
+	getters: {
+		toastToShow: (state: StateModel) => state.list.at(0) ?? null,
+	},
+	actions: {
+		showToast(message: string, options?: Partial<Omit<Toast, 'message'>>) {
+			this.$state.list.push({
+				message,
+				status: 'default',
+				timeout: defaultShowTime,
+				...options,
+			})
+		},
+		removeToast(index: number) {
+			this.$state.list.splice(index, 1)
+		},
+	},
+})
+
+if (import.meta.hot) {
+	import.meta.hot.accept(acceptHMRUpdate(useToastStore, import.meta.hot))
+}
